b9c73e01abfcbae204990e66f96bab720a8ce4dc
Ron G <62861315+rgultiano@users.noreply.github.com>
2021-08-10 18:58:01 +1000

    Updated to meet requirement of GET * rather than just GET /

--- server.js
+++ server.js
@@ -24,10 +24,6 @@ function appendNote(note){
 // serve up the public folder for access
 app.use(express.static('public'));
 
-app.get('/',(req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 app.get('/notes',(req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
@@ -45,6 +41,11 @@ app.post('/api/notes',(req, res) =>{
     // add note to file
     res.send(JSON.stringify(note));
 });
+
+
+app.get('*',(req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
